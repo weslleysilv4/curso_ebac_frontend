1122b3fa2fd3181e150e674fda07f986f46143df
Weslley Silva <weslleysilva.swe@gmail.com>
2023-01-12 14:31:20 -0300

    Commit inicial

---
+++ FormulárioCadastro/readme.md
@@ -0,0 +1,3 @@
+## Crie um formulário de cadastro, este formulário deverá conter os campos: nome completo, e-mail, telefone, CPF, endereço completo e CEP; 
+## Utilize o plugin jQueryMaskPlugin para aplicar máscara aos campos de CPF e CEP; 
+## Crie uma branchchamada exercício_plugins_jqueryno repositório do curso e nos envie o link através da plataforma. 
---
+++ FormulárioCadastro/src/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $('#tel').mask('(00) 00000-0000',{placeholder: '(00) 00000-0000'}, {reverse: true});
+    $('#cpf').mask('000.000.000-00',{placeholder: '000.000.000-00'})
+    $('#cep').mask('00000-000', {placeholder:'00000-000'});
+    
+    $('#form-x').validate({
+        rules:{
+            nome:{
+                required: true,
+                minlength: 4
+            },
+            email:{
+                required: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf:{
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            submitHandler: function() {
+                alert("Usuário cadastrado");
+            },
+            invalidHandler: function(event, validator){
+                let error = validator.numberOfInvalids();
+                if(error){
+                    alert(`Existem ${error} campos incorretos`)
+                }
+            }
+        }
+    })
+    
+
+    
+    // $('#submit').click(function(){
+    //     const dados = document.getElementsByClassName('dados-tabble');
+    //     const campos = dados.value;
+    //     console.log(campos);        
+    // })
+
+});
